e00460cc974c7dfd21a8ff8fad4cebe185e3b98b
Deadburn <80601286+Deadburn@users.noreply.github.com>
2022-11-25 02:23:52 +0300

    writing custom logger

--- src/index.js
+++ src/index.js
@@ -3,30 +3,23 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 
-
 import App from "./App";
 import { CartProvider } from "./contexts/cart.context";
-import { CategoriesProvider } from "./contexts/categories.context";
+// import { CategoriesProvider } from "./contexts/categories.context";
 // import { UserProvider } from "./contexts/user.context";
 import { store } from "./store/store";
 
 import "./index.scss";
 
-
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-  <Provider store={store}>
-    <BrowserRouter>
-
-        <CategoriesProvider>
-          <CartProvider>
-            <App />
-          </CartProvider>
-        </CategoriesProvider>
-
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <CartProvider>
+          <App />
+        </CartProvider>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
