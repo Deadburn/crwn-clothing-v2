fc7aa61ddb72c8b884b14003add151545b68d277
Deadburn <80601286+Deadburn@users.noreply.github.com>
2022-11-24 23:05:04 +0300

    selectors usage and helper func for this

--- src/index.js
+++ src/index.js
@@ -1,25 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+
 
 import App from "./App";
 import { CartProvider } from "./contexts/cart.context";
 import { CategoriesProvider } from "./contexts/categories.context";
-import { UserProvider } from "./contexts/user.context";
+// import { UserProvider } from "./contexts/user.context";
+import { store } from "./store/store";
 
 import "./index.scss";
+
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+  <Provider store={store}>
     <BrowserRouter>
-      <UserProvider>
+
         <CategoriesProvider>
           <CartProvider>
             <App />
           </CartProvider>
         </CategoriesProvider>
-      </UserProvider>
+
     </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -4,17 +4,19 @@ import { Outlet } from "react-router-dom";
 import CartIcon from "../../components/cart-icon/cart-icon.component";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 
-import { UserContext } from "../../contexts/user.context";
 import { CartContext } from "../../contexts/cart.context";
+import { selectCurrentUser } from "../../store/user/user.selector";
 
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 import { signOutUser } from "../../utils/firebase/firebase.utils.js";
 
 
 import {LogoContainer, NavigationContainer, NavLinks, NavLink} from "./navigation.styles";
+import { useSelector } from "react-redux";
+
 
 const Navigation = () => {
-  const { currentUser } = useContext(UserContext);
+  const currentUser =  useSelector(selectCurrentUser)
   const { isCartOpen } = useContext(CartContext)
 
   return (
