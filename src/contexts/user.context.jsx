fae78d82c18680cc5a30a8c85b2168f473eb8df8
Deadburn <80601286+Deadburn@users.noreply.github.com>
2022-11-24 15:20:23 +0300

    userContext reducer usage

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,6 +1,9 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useEffect, useReducer } from "react";
 
-import { createUserDocumentFromAuth, onAuthStateChangedListener } from "../utils/firebase/firebase.utils";
+import {
+  createUserDocumentFromAuth,
+  onAuthStateChangedListener,
+} from "../utils/firebase/firebase.utils";
 
 // as the actual value you want to access
 export const UserContext = createContext({
@@ -8,22 +11,66 @@ export const UserContext = createContext({
   setCurrentUser: () => null,
 });
 
+export const USER_ACTION_TYPES = {
+  SET_CURRENT_USER: "SET_CURRENT_USER",
+};
+
+const userReducer = (state, action) => {
+  console.log('dispatched')
+  console.log(action)
+  const { type, payload } = action;
+
+  switch (type) {
+    case USER_ACTION_TYPES.SET_CURRENT_USER:
+      return {
+        ...state,
+        currentUser: payload,
+      };
+
+    default:
+      throw new Error(`unhandled type ${type} in userReducer`);
+  }
+};
+
+const INITIAL_STATE = {
+  currentUser: null
+}
+
+
 export const UserProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null);
+  const [state, dispatch] = useReducer(userReducer, INITIAL_STATE)
+
+  const {currentUser} = state
+  console.log(currentUser)
+
+  const setCurrentUser = (user) => {
+    dispatch({type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user})
+  }
+
 
   const value = { currentUser, setCurrentUser };
 
-
   useEffect(() => {
     const unsubscribe = onAuthStateChangedListener((user) => {
-      if(user) {
+      if (user) {
         createUserDocumentFromAuth(user);
       }
       setCurrentUser(user);
-    })
+    });
 
-    return unsubscribe
-  }, [])
+    return unsubscribe;
+  }, []);
 
   return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
 };
+
+/*
+
+  const userReducer = (state, action) => {
+    return {
+      currentUser: null
+    }
+  }
+
+
+*/
